b63c05f2651d5446c32d08426aef6db9bf3bf65b
gunigma <spamy.inpg@gmail.com>
2017-04-20 12:20:47 +0200

    Update LOGIC.cpp

--- LOGIC.cpp
+++ LOGIC.cpp
@@ -8,8 +8,8 @@ int main(void)                                              //      MAIN
     int wczytywane;
     do
     {
-        cin>>wczytywane;
+        cin>>wczytywane; //hejo
         
 
-    } while (wczytywane != 0);
+    } while (wczytywane != 0); //ale gupie to
 }
